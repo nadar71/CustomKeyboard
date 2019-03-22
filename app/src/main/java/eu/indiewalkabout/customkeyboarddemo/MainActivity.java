b50c5fc86547a849f2d867433078713b9b50a65f
simone <simone.mapelli@gmail.com>
2019-03-22 16:56:03 +0100

    Basic with press enter  button detected

---
+++ app/src/main/java/eu/indiewalkabout/customkeyboarddemo/MainActivity.java
@@ -0,0 +1,36 @@
+package eu.indiewalkabout.customkeyboarddemo;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        MyKeyboard keyboard = (MyKeyboard) findViewById(R.id.keyboard);
+
+        // prevent system keyboard from appearing when EditText is tapped
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+        // pass the InputConnection from the EditText to the keyboard
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        keyboard.setInputConnection(ic);
+
+
+    }
+}
